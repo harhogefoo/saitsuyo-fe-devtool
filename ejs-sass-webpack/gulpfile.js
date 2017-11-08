96a981195f696eecdf97788e852549d61138600b
shino <masataka_shinohara@r.recruit.co.jp>
2017-11-08 16:42:24 +0900

    feat: minify, uglifyを追加。development、productionの分岐処理を追加

--- ejs-sass-webpack/gulpfile.js
+++ ejs-sass-webpack/gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp')
 const webpackStream = require('webpack-stream')
 const webpack = require('webpack')
-
 const ejs = require('gulp-ejs')
 const sass = require('gulp-sass')
+const minifycss = require('gulp-minify-css')
 const autoprefixer = require('gulp-autoprefixer')
 const notify = require('gulp-notify')
 const plumber = require('gulp-plumber')
 const browserSync = require('browser-sync')
+
+const gulpIf = require('gulp-if')
+const minimist = require('minimist')
 
 const paths = {
   'html': './public/',
@@ -18,16 +21,31 @@ const paths = {
   'out_js': './public/js/',
 }
 
-const sassOptions = {
-  outputStyle: 'expanded',
-  errLogToConsole: false,
+const envSettings = {
+  string: 'env',
+  default: {
+    env: process.env.NODE_ENV || 'development',
+  },
 }
 
-const webpackOptions = {
-  entry: paths.in_js + 'main.js',
-  output: {
-    filename: paths.out_js + 'app.bundle.js',
+// 実行時の引数を取り込む
+const options = minimist(process.argv.slice(2), envSettings)
+const production = options.env === 'production'
+
+const webpackPlugins = production ? [new webpack.optimize.UglifyJsPlugin()] : []
+const config = {
+  sassOptions: {
+    outputStyle: 'expanded',
+    errLogToConsole: false,
   },
+  webpackOptions: {
+    entry: paths.in_js + 'main.js',
+    output: {
+      filename: 'app.bundle.js',
+    },
+    plugins: webpackPlugins,
+  },
+  envProduction: production,
 }
 
 gulp.task('ejs', () => {
@@ -42,13 +60,14 @@ gulp.task('ejs', () => {
 gulp.task('sass', () => {
   gulp.src(paths.scss + '**/*.scss')
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-    .pipe(sass(sassOptions))
+    .pipe(sass(config.sassOptions))
     .pipe(autoprefixer())
+    .pipe(gulpIf(config.envProduction, minifycss()))
     .pipe(gulp.dest(paths.css))
 })
 
 gulp.task('webpack', () => {
-  return webpackStream(webpackOptions, webpack).pipe(gulp.dest(''))
+  return webpackStream(config.webpackOptions, webpack).pipe(gulp.dest(paths.out_js))
 })
 
 // Browser Sync
